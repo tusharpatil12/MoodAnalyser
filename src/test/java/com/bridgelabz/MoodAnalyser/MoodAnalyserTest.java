78a6d6dc9db062d56710d0d7a6c81e3ee1fd8a89
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 <tusharpatil22700@gmail.com>
2022-11-17 14:10:37 -0800

    initial commit

--- src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyserTest.java
@@ -6,21 +6,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
 public class MoodAnalyserTest {
-	
-	 @BeforeAll
-	    static void displayMsg() {
-	        System.out.println("Welcome Mood Analyzer Problem");
-	    }
 
-	    @AfterEach
-	    public void afterMsgDisplay() {
-	        System.out.println("Passed");
-	    }
-	    @Test
-	    public void givenMessage_WhenProper_ShouldReturnSad() {
-	        MoodAnalyser moodanalyser = new MoodAnalyser("I am in Sad Mood");
-	        String result = moodanalyser.analyseMood();
-	        Assertions.assertEquals(result,"Sad");
-	    }
+	@BeforeAll
+	static void displayMsg() {
+		System.out.println("Welcome Mood Analyzer Problem");
+	}
+
+	@AfterEach
+	public void afterMsgDisplay() {
+		System.out.println("Passed");
+	}
+	@Test
+	public void givenMessage_WhenProper_ShouldReturnSad() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in Sad Mood");
+		String result = moodanalyser.analyseMood();
+		Assertions.assertEquals(result,"Sad");
+	}
+
+	@Test
+	public void givenMessage_ContainsAny_init_ShouldReturnHappy() {
+		MoodAnalyser moodanalyser = new MoodAnalyser("I am in Happy Mood");
+		String result = moodanalyser.analyseMood();
+		Assertions.assertEquals(result,"Happy");
+	}
 
 }
