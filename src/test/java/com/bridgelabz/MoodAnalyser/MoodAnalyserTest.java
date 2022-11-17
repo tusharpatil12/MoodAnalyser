24faa8f3ce4d34e4a3970cfdafde93883f036a28
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 <tusharpatil22700@gmail.com>
2022-11-17 13:32:22 -0800

    welcome

---
+++ src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyserTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.MoodAnalyser;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MoodAnalyserTest {
+	
+	 @Test
+	    public void shouldAnswerWithTrue() {
+	        Assertions Assert = null;
+	        Assert.assertTrue(true);
+	    }
+
+}
