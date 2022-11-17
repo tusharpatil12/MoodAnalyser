ee52ed153eafaa3f6a6db3b7f5abcb5dda8a3be1
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 <tusharpatil22700@gmail.com>
2022-11-17 13:25:49 -0800

    welcome

---
+++ src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyser.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.MoodAnalyser;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to mood analyser program");
+
+	}
+
+}
